1e3c709f7e8485c0b7b86345f729cdd6f1ea7c16
Maciej Gębarski <mgebarski@gmail.com>
2023-06-14 22:06:21 +0200

    add base dotfiels

---
+++ README.md
@@ -0,0 +1,15 @@
+REQUIRED SOFT:
+oh-my-zsh
+feh
+variety
+nm-applet
+pamac-tray
+xfce4-power-manager
+volumeicon
+numlockx
+blueberry-tray
+picom
+polybar
+polkit-gnome-authentication-agent-1
+xfce4-notifyd
+dmenu
---
+++ xmonad/.xmonad/scripts/autostart.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+#Set your native resolution IF it does not exist in xrandr
+#More info in the script
+#run $HOME/.xmonad/scripts/set-screen-resolution-in-virtualbox.sh
+
+
+(sleep 2; run $HOME/.config/polybar/launch.sh) &
+
+
+#cursor active at boot
+xsetroot -cursor_name left_ptr &
+
+
+#Some ways to set your wallpaper besides variety or nitrogen
+feh --bg-fill /usr/share/backgrounds/archlinux/arch-wallpaper.jpg &
+feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
+
+#starting utility applications at boot time
+run variety &
+run nm-applet &
+run pamac-tray &
+run xfce4-power-manager &
+run volumeicon &
+numlockx on &
+blueberry-tray &
+picom --config $HOME/.xmonad/scripts/picom.conf &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+
+setxkbmap -option ctrl:nocaps -layout pl
